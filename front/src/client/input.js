b4d062294bb4113f35741d94745d306e0744f567
keyleeu <minhk.lee21@gmail.com>
2023-07-21 20:31:06 +0900

    1353251235

--- front/src/client/input.js
+++ front/src/client/input.js
@@ -1,8 +1,6 @@
 // Learn more about this file at:
 // https://victorzhou.com/blog/build-an-io-game-part-1/#6-client-input-%EF%B8%8F
 import { updateInputKeyBoardDown, updateInputKeyBoardUp } from './networking';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { enterKeyBoard } from './index';
 import { getCurrentState } from './state';
 
@@ -10,11 +8,6 @@ const Constants = require('../shared/constants');
 const canvas = document.getElementById('game-canvas');
 
 const { PLAYER_RADIUS, MAP_SIZE } = Constants;
-=======
->>>>>>> 89570e30828139c7783d459f7f210410be799700
-=======
-import { enterKeyBoard } from './index';
->>>>>>> 9ec07c8d260f69e705b5349e2bfeeefa3859fea8
 
 function onkeyDown(e) {
   if (e.keyCode === 87 || e.keyCode === 83 || e.keyCode === 68 || e.keyCode === 65) {
@@ -54,17 +47,12 @@ export function stopCapturingInput() {
   // window.removeEventListener('touchmove', onTouchInput);
   window.removeEventListener('keydown', onkeyDown);
   window.removeEventListener('keyup', onkeyUp);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 9ec07c8d260f69e705b5349e2bfeeefa3859fea8
 }
 
 /* ------------------------------------------------------------ */
 
 // 여기에 상대 플레이어를 마우스 클릭 하는 기능을 구현하고 싶어
 function onMouseInput(e) {
-<<<<<<< HEAD
   const rect = canvas.getBoundingClientRect();
   const x = e.clientX - rect.left;
   const y = e.clientY - rect.top;
@@ -80,8 +68,6 @@ function onMouseInput(e) {
 }
 
 function clickPlayer(x, y) {
-
-  
 
   console.log(x, y);
   // 플레이어의 목록을 가져옵니다. 이는 게임의 상태나 다른 곳에서 가져와야 합니다.
@@ -104,27 +90,4 @@ function clickPlayer(x, y) {
     }
   }
 
-  // 클릭한 위치가 어떤 플레이어의 영역 내에도 있지 않다면,
-  // 락온을 해제합니다.
-  // lockedPlayerId = null;
-
 }
-
-// function handleInput(x, y) {
-//   // const dir = Math.atan2(x - window.innerWidth / 2, window.innerHeight / 2 - y);
-//   const dir = Math.atan2(x - window.innerWidth / 2, window.innerHeight / 2 - y);
-//   updateDirection(dir);
-// }
-=======
-}
->>>>>>> 89570e30828139c7783d459f7f210410be799700
-=======
-  handleInput(e.clientX, e.clientY);
-}
-
-function handleInput(x, y) {
-  // const dir = Math.atan2(x - window.innerWidth / 2, window.innerHeight / 2 - y);
-  const dir = Math.atan2(x - window.innerWidth / 2, window.innerHeight / 2 - y);
-  updateDirection(dir);
-}
->>>>>>> 9ec07c8d260f69e705b5349e2bfeeefa3859fea8
