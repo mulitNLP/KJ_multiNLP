047602e05feb58d54892a76217126bb4a3691cd6
bknote71 <bknote71@gmail.com>
2023-07-21 13:30:28 +0900

    move

--- front/src/client/input.js
+++ front/src/client/input.js
@@ -1,33 +1,33 @@
 // Learn more about this file at:
 // https://victorzhou.com/blog/build-an-io-game-part-1/#6-client-input-%EF%B8%8F
-import { updateDirection, updateInputKeyBoard } from './networking';
+import { updateInputKeyBoardDown, updateInputKeyBoardUp } from './networking';
 
 function onkeyDown(e) {
   if (e.keyCode === 87 || e.keyCode === 83 || e.keyCode === 68 || e.keyCode === 65 || e.keyCode === 13) {
-    updateInputKeyBoard(e.keyCode, true);
+    updateInputKeyBoardDown(e.keyCode);
   }
 }
 
 function onkeyUp(e) {
   if (e.keyCode === 87 || e.keyCode === 83 || e.keyCode === 68 || e.keyCode === 65 || e.keyCode === 13) {
-    updateInputKeyBoard(e.keyCode, false);
+    updateInputKeyBoardUp(e.keyCode);
   }
 }
 
-function onMouseInput(e) {
-  handleInput(e.clientX, e.clientY);
-}
+// function onMouseInput(e) {
+//   handleInput(e.clientX, e.clientY);
+// }
 
-function onTouchInput(e) {
-  const touch = e.touches[0];
-  handleInput(touch.clientX, touch.clientY);
-}
+// function onTouchInput(e) {
+//   const touch = e.touches[0];
+//   handleInput(touch.clientX, touch.clientY);
+// }
 
-function handleInput(x, y) {
-  // const dir = Math.atan2(x - window.innerWidth / 2, window.innerHeight / 2 - y);
-  const dir = Math.atan2(x - window.innerWidth / 2, window.innerHeight / 2 - y);
-  updateDirection(dir);
-}
+// function handleInput(x, y) {
+//   // const dir = Math.atan2(x - window.innerWidth / 2, window.innerHeight / 2 - y);
+//   const dir = Math.atan2(x - window.innerWidth / 2, window.innerHeight / 2 - y);
+//   updateDirection(dir);
+// }
 
 export function startCapturingInput() {
   // window.addEventListener('mousemove', onMouseInput);
--- front/src/client/networking.js
+++ front/src/client/networking.js
@@ -1,49 +1,9 @@
 // Learn more about this file at:
 // https://victorzhou.com/blog/build-an-io-game-part-1/#4-client-networking
-import io from 'socket.io-client';
+// import io from 'socket.io-client';
 import { throttle } from 'throttle-debounce';
 import { processGameUpdate } from './state';
 
-const Constants = require('../shared/constants');
-
-const socketProtocol = (window.location.protocol.includes('https')) ? 'wss' : 'ws';
-const socket = io(`${socketProtocol}://${window.location.host}`, { reconnection: false });
-
-const connectedPromise = new Promise(resolve => {
-  socket.on('connect', () => {
-    console.log('Connected to server!');
-    resolve();
-  });
-});
-
-export const connect = onGameOver => (
-  connectedPromise.then(() => {
-    console.log(onGameOver);
-    // Register callbacks
-    socket.on(Constants.MSG_TYPES.GAME_UPDATE, processGameUpdate);
-    socket.on(Constants.MSG_TYPES.GAME_OVER, onGameOver);
-    socket.on('disconnect', () => {
-      console.log('Disconnected from server.');
-      document.getElementById('disconnect-modal').classList.remove('hidden');
-      document.getElementById('reconnect-button').onclick = () => {
-        window.location.reload();
-      };
-    });
-  })
-);
-
-export const play = username => {
-  console.log("playplayplay");
-  console.log(`username: ${username}`);
-  socket.emit(Constants.MSG_TYPES.JOIN_GAME, username);
-};
-
-export const updateDirection = throttle(20, dir => {
-  console.log(`dir: ${dir}`)
-  socket.emit(Constants.MSG_TYPES.INPUT, dir);
-});
-
-<<<<<<< HEAD
 // websocket connection
 const roomId = 1;
 const websocket = new WebSocket(`ws://localhost:8080/room/${roomId}`);
@@ -51,16 +11,16 @@ const websocket = new WebSocket(`ws://localhost:8080/room/${roomId}`);
 const wsconnectedPromise = new Promise(resolve => {
   // to websocket, 이벤트 핸들러 변경
   // io 와는 다르게 WebSocket 을 사용할 때는 이벤트 핸들러를 직접 등록해야 한다.
-  websocket.onopen = ((e) => {
+  websocket.onopen = (() => {
     console.log('Connected to web socket game server!');
-    console.log(e);
     resolve();
-  })
+  });
 });
 
+let myId;
 
 // connect 이후 콜백 등록
-export const wsconnect = onGameOver => (
+export const connect = onGameOver => (
   wsconnectedPromise.then(() => {
     // Register callbacks
     // socket.on(Constants.MSG_TYPES.GAME_UPDATE, processGameUpdate);
@@ -68,16 +28,60 @@ export const wsconnect = onGameOver => (
     // 
     websocket.onmessage = event => {
       const message = JSON.parse(event.data);
-      console.log(message);
 
-      if (message.type == "sentergame") {
-        console.log("enter game!!!!!!!!!!!!!!!!!1");
+      if (message.type === 'sentergame') {
+        // console.log('enter game!!');
+        // console.log(message.update);
+        myId = message.player.name;
+        const update = {
+          me: message.update.infos[0],
+          leaderboard: [],
+          t: message.update.t,
+        };
+        processGameUpdate(update);
+      } else if (message.type === 'sspawn') {
+        // console.log('spawn!!');
+        // processGameUpdate(message);
+      } else if (message.type === 'supdate') { // update (움직임 패킷)
+        // 플레이어 update
+        // me 와 others 구분
+        let me;
+        const others = [];
+
+        for (let item of message.update.infos) {
+          // console.log(item);
+          // console.log(myId);
+          if (item['id'] === myId) {
+            me = item;
+          }
+          else {
+            others.push(item);
+          }
+        }
+
+        const update = {
+          bullets: [],
+          leaderboard: [],
+          t: message.update.t,
+          me: me,
+          others: others,
+        };
+
+        // console.log('update!');
+        // console.log(update);
+
+        processGameUpdate(update);
+
+      } else if (message.type === 'smove') { // move update (움직임 패킷)
+        // processGameUpdate(message.update);
+        // 일단은 무시 (나중에 bullet, meteor 처리 시에 사용될 무브)
+      } else if (message.type === 'schat') {
+        // console.log('schat');
+      } else if (message.type === 'sskill') {
+        // console.log('sskill');
+      } else if (message.type === 'sdie') {
+        onGameOver();
       }
-      else if (message.type == "smove") { // move update (움직임 패킷)
-        console.log("movemovemove!!");
-
-      }
-
     };
 
     websocket.onclose = () => {
@@ -86,41 +90,72 @@ export const wsconnect = onGameOver => (
       document.getElementById('reconnect-button').onclick = () => {
         window.location.reload();
       };
-    }
+    };
   })
 );
 
 // send data << 어차피 이거 아님
-export const wsplay = username => {
+export const play = name => {
   const message = {
-    type: "centergame",
-    protocol: "C_EnterGame",
-    username: username,
+    type: 'centergame',
+    protocol: 'C_EnterGame',
+    username: name,
   };
   websocket.send(JSON.stringify(message));
-}
+};
 
-// move
-export const wsupdateDirection = throttle(20, dir => {
-  console.log("ws move");
+export const updateInputKeyBoardDown = throttle(20, (key) => {
+  var dir;
+  if (key === 87) {
+    dir = 'North';
+  } else if (key === 83) {
+    dir = 'South';
+  } else if (key === 68) {
+    dir = 'East';
+  } else if (key === 65) {
+    dir = 'West';
+  }
+
   const message = {
-    type: "cmove",
-    protocol: "C_Move",
+    type: 'cmove',
+    protocol: 'C_Move',
     posInfo: {
-      pos: {
-        x: 123.5,
-        y: 123.5
-      },
-      dir: "North",
-      state: "IDLE"
-    }
+      pos: {},
+      dirs: [],
+      state: null,
+    },
+    dir: dir,
+    updown: true,
   };
 
+  console.log(`message: ${JSON.stringify(message)}`);
   websocket.send(JSON.stringify(message));
 });
 
-/* -------------------------------------------------- */
+export const updateInputKeyBoardUp = (key) => {
+  var dir;
+  if (key === 87) {
+    dir = 'North';
+  } else if (key === 83) {
+    dir = 'South';
+  } else if (key === 68) {
+    dir = 'East';
+  } else if (key === 65) {
+    dir = 'West';
+  }
 
-export const updateInputKeyBoard = throttle(20 , (key,updown) => {
-  socket.emit(Constants.MSG_TYPES.INPUTKEYBOARD, key,updown);
-});
+  const message = {
+    type: 'cmove',
+    protocol: 'C_Move',
+    posInfo: {
+      pos: {},
+      dirs: [],
+      state: null,
+    },
+    dir: dir,
+    updown: false,
+  };
+
+  console.log(`message: ${JSON.stringify(message)}`);
+  websocket.send(JSON.stringify(message));
+};
