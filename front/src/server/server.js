276f254ecb7a474d4fe107f6f97a100bd33639e7
creamcheezecat <92340465+creamcheezecat@users.noreply.github.com>
2023-07-22 16:50:10 +0900

    코드 정리

--- front/src/server/server.js
+++ front/src/server/server.js
@@ -2,9 +2,6 @@ const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const socketio = require('socket.io');
-
-const Constants = require('../shared/constants');
-const Game = require('./game');
 const webpackConfig = require('../../webpack.dev.js');
 
 // Setup an Express server
@@ -31,28 +28,5 @@ const io = socketio(server);
 // Listen for socket.io connections
 io.on('connection', socket => {
   console.log('Player connected!', socket.id);
-
-  socket.on(Constants.MSG_TYPES.INPUTKEYBOARD, handleInputKeyBoard);
-  socket.on(Constants.MSG_TYPES.JOIN_GAME, joinGame);
-  socket.on(Constants.MSG_TYPES.INPUT, handleInput);
   socket.on('disconnect', onDisconnect);
 });
-
-// Setup the Game
-const game = new Game();
-
-function joinGame(username) {
-  game.addPlayer(this, username);
-}
-
-function handleInput(dir) {
-  game.handleInput(this, dir);
-}
-
-function onDisconnect() {
-  game.removePlayer(this);
-}
-
-function handleInputKeyBoard(key, updown) {
-  game.handleInputKeyBoard(this, key, updown);
-}
--- front/src/shared/constants.js
+++ front/src/shared/constants.js
@@ -1,23 +1,5 @@
 module.exports = Object.freeze({
   PLAYER_RADIUS: 30,
-  PLAYER_MAX_HP: 100,
-  PLAYER_SPEED: 200,
-  PLAYER_FIRE_COOLDOWN: 0.25,
-
   BULLET_RADIUS: 3,
-  BULLET_SPEED: 800,
-  BULLET_DAMAGE: 10,
-
-  SCORE_BULLET_HIT: 20,
-  SCORE_PER_SECOND: 1,
-
   MAP_SIZE: 2000,
-  METEOR_SPEED:1,
-  MSG_TYPES: {
-    JOIN_GAME: 'join_game',
-    GAME_UPDATE: 'update',
-    INPUT: 'input',
-    GAME_OVER: 'dead',
-    INPUTKEYBOARD: 'inputkeyboard',
-  },
 });
