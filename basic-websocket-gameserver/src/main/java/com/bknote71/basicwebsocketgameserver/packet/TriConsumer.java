15c7d3f4b346e22ea28639635fd2423a168c41c3
bknote71 <bknote71@gmail.com>
2023-07-23 15:54:02 +0900

    change project name

--- basic-websocket-gameserver/src/main/java/com/bknote71/basicwebsocketgameserver/packet/TriConsumer.java
+++
@@ -1,5 +0,0 @@
-package com.bknote71.basicwebsocketgameserver.packet;
-
-public interface TriConsumer<T, T1, T2> {
-    void accept(T t, T1 t1, T2 t2);
-}
--- basic-websocket-gameserver/src/main/java/com/bknote71/basicwebsocketgameserver/protocol/info/ProjectileInfo.java
+++
@@ -1,9 +0,0 @@
-package com.bknote71.basicwebsocketgameserver.protocol.info;
-
-import lombok.Data;
-
-@Data
-public class ProjectileInfo {
-    private double speed;
-    private double range;
-}
